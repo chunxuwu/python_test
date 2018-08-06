5d8c2dfef1247a755e038ba615651fdecaaab014
chunxuwu <chunxuwu_zjut@163.com>
2018-08-06 11:00:26 +0800

    Initial commit

---
+++ 4_2_2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  6 09:57:02 2018
+
+@author: NEVERGUVEIP
+"""
+
+spam=['apple','bananas','tofu','cats']
+for i in range(len(spam)-1):
+    print(spam[i],end=',')
+print('and '+spam[-1])
